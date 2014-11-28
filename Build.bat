c398b031830fe8025275f55264a20784e7c1b120
pzaps <pikablu@outlook.com>
2014-11-28 15:53:38 +0000

    Switched to using master framework repository

--- Build.bat
+++
@@ -1,6 +0,0 @@
-set bb.build.msbuild.exe=
-for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild.exe=%%D\MSBuild.exe
-
-%msbuild.exe% "framework-net/Net.sln"
-%msbuild.exe% "Installer.sln"
-Pause
